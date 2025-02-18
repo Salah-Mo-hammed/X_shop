3d72f1370f86aad95e3f86a756c88b9bb20bee3f
Salah-Mo-hammed <sm4613283@gmail.com>
2025-02-19 00:50:51 +0200

    Updated project dependencies and fixed cart functionality

--- lib/features/cart/data/data_source/remote/cart_data_source.dart
+++ lib/features/cart/data/data_source/remote/cart_data_source.dart
@@ -6,56 +6,63 @@ import 'package:e_commerce_app/features/e_commerce_clean/data/models/product_mod
 abstract class CartDataSource {
   Future<Either<Failure, Unit>> addToCart(String userId, ProductModel product);
   Future<Either<Failure, bool>> checkAdded(String userId, ProductModel product);
-  Future<Either<Failure, Unit>> deleteFromCart(
-      String userId, ProductModel productId);
+  Future<Either<Failure, Unit>> deleteFromCart(String userId, ProductModel product);
   Future<Either<Failure, List<ProductModel>>> getCartProducts(String userId);
 }
 
 class FireStore implements CartDataSource {
-  @override
-  Future<Either<Failure, Unit>> addToCart(
-      String userId, ProductModel product) async {
-    FirebaseFirestore firestore = FirebaseFirestore.instance;
-    Map<String, dynamic> productData = product.toJson();
-    await firestore.collection('carts').doc(userId).update({
-      'productsId': FieldValue.arrayUnion([productData])
-    });
-    return const Right(unit);
-  }
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   @override
-  Future<Either<Failure, Unit>> deleteFromCart(
-      String userId, ProductModel product) async {
-    FirebaseFirestore firestore = FirebaseFirestore.instance;
-    await firestore.collection('carts').doc(userId).update({
-      'productsId': FieldValue.arrayRemove([product.toJson()])
-    });
-    return const Right(unit);
-  }
-
-  @override
-  Future<Either<Failure, List<ProductModel>>> getCartProducts(
-      String userId) async {
+  Future<Either<Failure, Unit>> addToCart(String userId, ProductModel product) async {
     try {
-      FirebaseFirestore firestore = FirebaseFirestore.instance;
+      DocumentReference userCartRef = firestore.collection('carts').doc(userId);
+      DocumentSnapshot userCartSnapshot = await userCartRef.get();
 
-      final DocumentSnapshot<Map<String, dynamic>> data =
-          await firestore.collection('carts').doc(userId).get();
+      if (!userCartSnapshot.exists) {
+        await userCartRef.set({'productsId': []});
+      }
 
-      if (!data.exists || data.data() == null) {
+      await userCartRef.update({
+        'productsId': FieldValue.arrayUnion([product.toJson()])
+      });
+      return const Right(unit);
+    } catch (e) {
+      return Left(DatabaseFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Unit>> deleteFromCart(String userId, ProductModel product) async {
+    try {
+      DocumentReference userCartRef = firestore.collection('carts').doc(userId);
+      DocumentSnapshot userCartSnapshot = await userCartRef.get();
+
+      if (!userCartSnapshot.exists || userCartSnapshot.data() == null) {
         return const Left(DatabaseFailure("Cart not found"));
       }
 
-      List<dynamic> productsDataList =
-          data.data()!['productsId'] as List<dynamic>;
+      await userCartRef.update({
+        'productsId': FieldValue.arrayRemove([product.toJson()])
+      });
+      return const Right(unit);
+    } catch (e) {
+      return Left(DatabaseFailure(e.toString()));
+    }
+  }
 
-      if (productsDataList.isEmpty) {
-        return const Right([]);
+  @override
+  Future<Either<Failure, List<ProductModel>>> getCartProducts(String userId) async {
+    try {
+      DocumentSnapshot<Map<String, dynamic>> data =
+          await firestore.collection('carts').doc(userId).get();
+
+      if (!data.exists || data.data() == null || !data.data()!.containsKey('productsId')) {
+        return const Left(DatabaseFailure("Cart not found"));
       }
 
-      List<ProductModel> products = productsDataList
-          .map((productData) => ProductModel.fromJson(productData))
-          .toList();
+      List<dynamic> productsDataList = data.data()!['productsId'] as List<dynamic>;
+      List<ProductModel> products = productsDataList.map((productData) => ProductModel.fromJson(productData)).toList();
 
       return Right(products);
     } catch (e) {
@@ -64,20 +71,16 @@ class FireStore implements CartDataSource {
   }
 
   @override
-  Future<Either<Failure, bool>> checkAdded(
-      String userId, ProductModel product) async {
+  Future<Either<Failure, bool>> checkAdded(String userId, ProductModel product) async {
     try {
-      FirebaseFirestore firestore = FirebaseFirestore.instance;
-
-      final DocumentSnapshot<Map<String, dynamic>> data =
+      DocumentSnapshot<Map<String, dynamic>> data =
           await firestore.collection('carts').doc(userId).get();
 
-      if (!data.exists || data.data() == null) {
+      if (!data.exists || data.data() == null || !data.data()!.containsKey('productsId')) {
         return const Right(false);
       }
 
       List<dynamic> productsList = data.data()!['productsId'] as List<dynamic>;
-
       bool isAdded = productsList.any((p) => p['id'] == product.id);
 
       return Right(isAdded);
